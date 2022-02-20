a489a1a7587638ba0842ae8fe685833f970c2a87
Md. Yeasir Arafat <md.yeasirarafat@Mds-MacBook-Pro.local>
2022-02-20 23:30:19 +0600

    first commit

---
+++ cherry_server.py
@@ -0,0 +1,20 @@
+import sys
+import cherrypy
+from mako.template import Template
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class Todo(object):
+    @cherrypy.expose
+    def index(self):
+        mytemplate = Template(filename='template/index.html')
+        return mytemplate.render()
+
+    @cherrypy.expose
+    def store(self, desc, date):
+        return desc
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(Todo(), '/')
